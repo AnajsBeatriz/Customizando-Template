689b6101666c8e78a8e2881e7d18d46c52640a0a
AnajsBeatriz <134506676+AnajsBeatriz@users.noreply.github.com>
2023-11-21 17:26:39 -0300

    estilização e adição de nova page
    login ainda não foi testado

--- src/components/header.js
+++ src/components/header.js
@@ -5,30 +5,29 @@ import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
-//import MenuIcon from '@mui/icons-material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
  
 function Header() {
     return (
-        <Box sx={{ flexGrow: 1 }}>
-        <AppBar position="static">
-          <Toolbar>
-            <IconButton
-              size="large"
-              edge="start"
-              color="inherit"
-              aria-label="menu"
-              sx={{ mr: 2 }}
-            >
-              {/*  
-              <MenuIcon />
-              */}
-            </IconButton>
-            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-              Gerenciamento de Projetos
-            </Typography>
-          </Toolbar>
-        </AppBar>
-      </Box>
+      <Box sx={{ flexGrow: 1, bgcolor:'#4a148c' }}>
+      <AppBar position="static" sx={{ bgcolor:'#6a1b9a' }}>
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1}}>
+            Gerenciamento de Projetos
+          </Typography>
+          <Button color="inherit">Sair</Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
     );
 }
  
